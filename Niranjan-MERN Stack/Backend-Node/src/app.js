dc2dbb9de665ac4af565815fbd0cd8ca50f3f492
Niranjannaresh <89674974+Niranjannaresh@users.noreply.github.com>
2022-01-09 16:56:11 +0530

    Add files via upload

---
+++ Niranjan-MERN Stack/Backend-Node/src/app.js
@@ -0,0 +1,20 @@
+
+const  connectDb =require("../src/db/connectDb")
+const express = require('express')
+const router= express.Router();
+// const route = require("../src/route/route")
+
+const app = express()
+const mongo =require("mongoose");
+
+const cors = require("cors");
+app.use(cors());
+// app.use(route)
+connectDb();
+
+app.use(express.json());
+app.use(require('./controler/ProductsApi/addProducts'))
+
+app.listen(5000, () => {
+    console.log(`server is runnig at port no 5000`);
+})
